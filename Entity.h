1c0b2f666f6f765881c6bd6b197f5b051a56b9a4
Niklas Vågstedt <niklasva@gmail.com>
2014-09-15 12:00:47 +0200

    Start

---
+++ Entity.h
@@ -0,0 +1,21 @@
+#include <string>
+#include <vector>
+
+#ifndef ENTITY
+#define ENTITY
+class Entity
+{
+public:
+Entity(const std::string& name, const std::string& description): 
+description_{description}, name_{name}
+    ~Entity() = default;
+    
+// Övriga konstruktorer är implicita.  
+std::string get_description() const {return description_;}
+std::string get_name() const {return name_;}
+private:
+const std::string name_,description_;
+}
+
+#endif
+
---
+++ Room.h
@@ -0,0 +1,18 @@
+#include "Entity.h"
+#ifndef ROOM
+#define ROOM
+
+class Room : public  Entity 
+{
+public:
+Room(const std::string& name, const std::string& description) : Entity (name_, description_){} // att se över.
+
+    void set_persons(const Person& p) { persons_.push_back(p); }
+    void set_objets(const Object& o) { objects_.push_back(p); }
+
+private: // Innehåller personer och objekt i rummet.
+    std::vector<Person> persons_;
+    std::vector<Object> objects_;
+    int  n{0},w{0},s{0},e{0};
+    int exits = [n,s,w,e]; 
+}
